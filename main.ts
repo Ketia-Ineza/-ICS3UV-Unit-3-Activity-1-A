53d521a5e49a8ffc3a47073fbebf4a8d828e43ce
default <ketia.ineza@stu.ocsb.ca>
2025-11-24 18:45:59 +0000

    Added variable declarations

---
+++ main.ts
@@ -0,0 +1,32 @@
+/**
+ * @author Ketia
+ * @version 1.0.0
+ * @date 2025-11-24
+ * @fileoverview This program shows variable declarations.
+ */
+
+// number data type
+let count: number = 20;
+
+// number data type big_number
+let big_number: number = 14500000000;
+
+// number data type temperature
+let temperature: number = -32.4;
+
+// number data type moLeCules
+let moLeCules: number = 8;
+
+// string data type
+let courseCode: string = "ICS3U";
+
+// boolean data type
+let playAgain: boolean = true;
+
+// display each data type to the screen
+console.log("The current count is: " + count + "?");
+console.log("This is a really big number: " + big_number);
+console.log("The outside temperature currently is: " + temperature + "°C");
+console.log("Out of water might have " + moLeCules + " moLeCules");
+console.log("True/False Computer programming is cool: " + playAgain);
+console.log("\nDone.");
